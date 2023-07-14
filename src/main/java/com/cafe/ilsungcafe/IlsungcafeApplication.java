7188424688f71dfbe1ff8952e2be8d7598f6ec32
jieunlee0106 <jieun3181@gmail.com>
2023-07-14 14:40:48 +0900

    홈 메뉴 카테고리 설정

---
+++ src/main/java/com/cafe/ilsungcafe/IlsungcafeApplication.java
@@ -0,0 +1,13 @@
+package com.cafe.ilsungcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlsungcafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlsungcafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cafe/ilsungcafe/IlsungcafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe.ilsungcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlsungcafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
